1db4595ce8d70d915c126a186634c1626555b3be
AymanYac <ayman.yachaoui@neonec.com>
2020-11-09 14:21:24 +0100

    V4.2 repack

--- src/transversal/dialog_toolbox/CaracDeclarationDialog.java
+++ src/transversal/dialog_toolbox/CaracDeclarationDialog.java
@@ -1,6 +1,5 @@
 package transversal.dialog_toolbox;
 
-import com.sun.javafx.scene.control.skin.BehaviorSkinBase;
 import controllers.Char_description;
 import controllers.paneControllers.TablePane_CharClassif;
 import javafx.application.Platform;
